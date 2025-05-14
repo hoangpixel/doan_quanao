ff3ba0d5a443f34c412767a51f9fe581959fa748
ving <ndvinh360@gmail.com>
2025-05-14 18:23:08 +0700

    sửa kiểm tra trùng mã loại + thêm số lượng cho chi tiết sản phẩm

--- src/BUS/LoaiSanPhamBUS.java
+++ src/BUS/LoaiSanPhamBUS.java
@@ -13,57 +13,58 @@ import java.util.ArrayList;
  * @author suvie
  */
 public class LoaiSanPhamBUS {
-    
+
     public static ArrayList<LoaiSanPhamDTO> dslsp;
-    
+
     public boolean kiemTraMaLoai(int malsp) {
         LoaiSanPhamDTO lsp = layLSPTheoMa(malsp);
         return lsp != null;
     }
-    
+
     public ArrayList<LoaiSanPhamDTO> docDSLSP() {
-        if(dslsp == null) {
+        if (dslsp == null) {
             dslsp = new LoaiSanPhamDAO().docDSLSP();
         }
         return dslsp;
     }
-    
+
     public void them(LoaiSanPhamDTO lsp) {
-        if(dslsp == null) {
+        if (dslsp == null) {
             dslsp = new ArrayList<>();
         }
-        lsp.setMaLoai(new LoaiSanPhamDAO().them(lsp));
+
+        new LoaiSanPhamDAO().them(lsp);
         dslsp.add(lsp);
     }
-    
+
     public void sua(LoaiSanPhamDTO lsp) {
         new LoaiSanPhamDAO().sua(lsp);
-        for(LoaiSanPhamDTO lsp1 : dslsp) {
-            if(lsp1.getMaLoai() == lsp.getMaLoai()) {
+        for (LoaiSanPhamDTO lsp1 : dslsp) {
+            if (lsp1.getMaLoai() == lsp.getMaLoai()) {
                 lsp1.setTenLoai(lsp.getTenLoai());
                 break;
             }
         }
     }
-    
+
     public void xoa(int malsp) {
         new LoaiSanPhamDAO().xoa(malsp);
         dslsp.removeIf(n -> n.getMaLoai() == malsp);
     }
-    
+
     public LoaiSanPhamDTO layLSPTheoMa(int malsp) {
         return new LoaiSanPhamDAO().layLSPTheoMaLoai(malsp);
     }
-    
+
     public static ArrayList<LoaiSanPhamDTO> getDSLSP() {
         return dslsp;
     }
-    
+
     public ArrayList<LoaiSanPhamDTO> timKiemTheoMa(int malsp) {
         ArrayList<LoaiSanPhamDTO> ds = new ArrayList<>();
         if (dslsp != null) {
-            for(LoaiSanPhamDTO lsp : dslsp) {
-                if(lsp.getMaLoai() == malsp) {
+            for (LoaiSanPhamDTO lsp : dslsp) {
+                if (lsp.getMaLoai() == malsp) {
                     ds.add(lsp);
                     break;
                 }
@@ -71,16 +72,29 @@ public class LoaiSanPhamBUS {
         }
         return ds;
     }
-    
+
     public ArrayList<LoaiSanPhamDTO> timKiemTheoTen(String ten) {
         ArrayList<LoaiSanPhamDTO> ds = new ArrayList<>();
         if (ten != null && !ten.isEmpty()) {
-           for (LoaiSanPhamDTO lsp : dslsp) {
+            for (LoaiSanPhamDTO lsp : dslsp) {
                 if (lsp.getTenLoai().toLowerCase().contains(ten.toLowerCase())) {
-                        ds.add(lsp);
+                    ds.add(lsp);
                 }
             }
         }
         return ds;
+    }
+
+    public boolean kiemTraTrungMa(int maLoai) {
+        if (dslsp == null) {
+            docDSLSP();
+
+        }
+        for (LoaiSanPhamDTO lsp : dslsp) {
+            if (lsp.getMaLoai() == maLoai) {
+                return true;
+            }
+        }
+        return false;
     }
 }
